7d30dda2fa223cf9f18cb2cae947920c1aa93e1a
Colin Brennan <cbrennan545@utexas.edu>
2021-08-30 12:56:31 -0400

    MOOSE App Development Step 4 - "Defined new input parameters, permeability and viscosity, for DarcyPressure kernel

--- src/kernels/DarcyPressure.C
+++ src/kernels/DarcyPressure.C
@@ -8,14 +8,23 @@ DarcyPressure::validParams()
 {
     InputParameters params = ADKernelGrad::validParams();
     params.addClassDescription("Compute the diffusion term for Darcy pressure ($p$) equation: " "$-\\nabla \\cdot \\frac{\\mathbf{K}}{\\mu} \\nabla p = 0$");
+    
+    // Add params to those required by default in ADKernelGrad. For pressure-diffusion, add perm. and visc. Permeability must be supplied by the user, and the viscosity will be defaulted to a value for water.
 
+    params.addRequiredParam<Real>("permeability", "The isentropic permeability ($K$) of the medium.");
+    params.addParam<Real>(
+        "viscosity",
+        7.98e-04,
+        "The dynamic viscosity ($\\mu$) of the fluid, the default value is that of water at 30 "
+        "degrees Celcius (7.98e-04 Pa-s).");
+    
     return params;
 }
 
 // constructor
 DarcyPressure::DarcyPressure(const InputParameters & parameters) : ADKernelGrad(parameters),
-    _permeability(0.8451e-09),
-    _viscosity(7.98e-04)
+    _permeability(getParam<Real>("permeability")),
+    _viscosity(getParam<Real>("viscosity"))
 {
     
 }
